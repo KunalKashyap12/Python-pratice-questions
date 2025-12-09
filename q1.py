9d219c917c10080b7b077b05bb96b1d48336b99b
Kunal Kashyap <kunalkashyap5301@gmail.com>
2025-12-09 22:51:03 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,7 @@
+#  for taking input from user
+n = int(input("Enter a number: "))
+# for checking if the number is even or odd.
+if n % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.") 
