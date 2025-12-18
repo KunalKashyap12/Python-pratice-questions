66585e7e4fec39698f50fa2f68d0a4d95edacca1
Kunal Kashyap <kunalkashyap5301@gmail.com>
2025-12-18 11:25:22 +0530

    Add files via upload

---
+++ q15.py
@@ -0,0 +1,14 @@
+# to sort an array.
+array = [3, 1, 4, 1, 5, 9]
+
+print(sorted(array))
+# in python using sort function.
+
+# to assume array and sorting by bubble sort.
+# for i in range(len(array)-1):
+#     for j in range(len(array)-i-1):
+#         if array[j]>array[j+1]:
+#             temp = array[j]
+#             array[j] = array[j+1]
+#             array[j+1] = temp
+# print(array)
